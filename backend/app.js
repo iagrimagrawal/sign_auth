fb3129874d38cd1bdd3fc544c82fc4dd7daddd8a
iagrimagrawal <agrimagrawal2479@gmail.com>
2025-03-28 19:02:32 +0530

    cross done

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ const express = require('express')
 const authRouter = require('./router/authRoute')
 const databaseconnect = require('./config/databaseConfig')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 const app = express()
 
 databaseconnect()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: [process.env.CLIENT_URL],
+    credentials: true
+}))
 
 
 app.use('/api/auth',authRouter);
